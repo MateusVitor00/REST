08b4a336302291ea735a96be9527b3c36cd4ed63
Mateus Vitor <mateusvitor01.mv@gmail.com>
2023-09-19 20:41:15 -0300

    Usando a biblioteca express, na criação do servidor remoto

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Iai');
+import express, { Request, Response, NextFunction } from 'express'
+
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
+    res.status(200).send({ foo: 'Sucesso Total'})
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
